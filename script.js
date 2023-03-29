dc79dffdc5c76d962fdece7d8899c85d2c7c78c3
Jo Keeley <student@Souths-MBP-7.lan>
2023-03-29 10:12:31 -0500

    added content

---
+++ script.js
@@ -0,0 +1,21 @@
+// Check the link in console
+// console.log("Hello World")
+
+// Guess That Number!
+// Message to be used through project
+const enterNumText = 'Please enter a number greater than zero'; // console.log(enterNumText);
+
+// For restarting the game
+let restartGame = true;
+
+// For starting the range of number to be guessed
+let rangeNum;
+
+// For storing the number of attempts the user has left
+let lives;
+
+// For storing the user's guess
+let guess;
+
+// For storing the user's response to play again or not
+let playAgain;
